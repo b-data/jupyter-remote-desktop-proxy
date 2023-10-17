ef4fca22146839550ae85a313cedf739257d9e45
Olivier Benz <olivier.benz@b-data.ch>
2023-10-17 08:36:24 +0200

    Remove TigerVNC remnants

--- jupyter_remote_desktop_proxy/__init__.py
+++ jupyter_remote_desktop_proxy/__init__.py
@@ -1,16 +1,11 @@
 import os
 import shlex
-import tempfile
 from shutil import which
 
 HERE = os.path.dirname(os.path.abspath(__file__))
 
 
 def setup_desktop():
-    # make a secure temporary directory for sockets
-    # This is only readable, writeable & searchable by our uid
-    sockets_dir = tempfile.mkdtemp()
-    sockets_path = os.path.join(sockets_dir, 'vnc-socket')
     vncserver = which('vncserver')
 
     vnc_args = [vncserver]
