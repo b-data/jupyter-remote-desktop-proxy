73848571ea8f6938aa220452edf66c0ebddc7ce0
Olivier Benz <olivier.benz@b-data.ch>
2025-03-14 19:30:11 +0100

    Revert "Modify JS and CSS for noVNC/vnc.html"
    This reverts commit 02a58cd64c802cfcf29339d08c44d0be1f600fde.

--- jupyter_remote_desktop_proxy/share/web/noVNC/app/ui.js
+++ jupyter_remote_desktop_proxy/share/web/noVNC/app/ui.js
@@ -4,9 +4,6 @@
  * Licensed under MPL 2.0 (see LICENSE.txt)
  *
  * See README.md for usage and integration instructions.
- *
- * Modified by b-data for seamless use with Jupyter Remote Desktop Proxy.
- *
  */
 
 import * as Log from '../core/util/logging.js';
@@ -136,7 +133,7 @@ const UI = {
 
         document.documentElement.classList.remove("noVNC_loading");
 
-        let autoconnect = UI.getSetting('autoconnect', true);
+        let autoconnect = UI.getSetting('autoconnect');
         if (autoconnect === 'true' || autoconnect == '1') {
             autoconnect = true;
             UI.connect();
@@ -181,16 +178,16 @@ const UI = {
         UI.initSetting('password');
         UI.initSetting('autoconnect', false);
         UI.initSetting('view_clip', false);
-        UI.initSetting('resize', 'scale');
+        UI.initSetting('resize', 'off');
         UI.initSetting('quality', 6);
         UI.initSetting('compression', 2);
         UI.initSetting('shared', true);
         UI.initSetting('bell', 'on');
         UI.initSetting('view_only', false);
         UI.initSetting('show_dot', false);
-        UI.initSetting('path', window.location.pathname.replace(/[^/]*$/, '').substring(1) + 'websockify');
+        UI.initSetting('path', 'websockify');
         UI.initSetting('repeaterID', '');
-        UI.initSetting('reconnect', true);
+        UI.initSetting('reconnect', false);
         UI.initSetting('reconnect_delay', 5000);
     },
     // Adds a link to the label elements on the corresponding input elements
