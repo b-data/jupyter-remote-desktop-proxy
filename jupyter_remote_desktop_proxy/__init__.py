5cb2c594e9d89769ab20440d661332b659d48349
Olivier Benz <olivier.benz@b-data.ch>
2026-02-23 17:50:09 +0100

    Test unix socket

--- jupyter_remote_desktop_proxy/__init__.py
+++ jupyter_remote_desktop_proxy/__init__.py
@@ -11,10 +11,13 @@ def setup_desktop():
     with open(vncserver) as vncserver_file:
         is_turbovnc = "turbovnc" in vncserver_file.read().casefold()
 
+    # {unix_socket} is expanded by jupyter-server-proxy
+    websockify_args = ['--unix-target', "{unix_socket}"]
+    vnc_args = [vncserver, '-rfbunixpath', "{unix_socket}", '-rfbport', '-1']
     if is_turbovnc:
-        vnc_args = [vncserver, '-noserverkeymap', '-localhost', '-rfbport', '{port}']
-    else:
-        vnc_args = [vncserver, '-rfbport', '{port}']
+        # turbovnc doesn't handle being passed -rfbport -1, but turbovnc also
+        # defaults to not opening a TCP port which is what we want to ensure
+        vnc_args = [vncserver, '-noserverkeymap', '-rfbunixpath', "{unix_socket}"]
 
     xstartup = os.getenv("JUPYTER_REMOTE_DESKTOP_PROXY_XSTARTUP")
     if not xstartup and not os.path.exists(os.path.expanduser('~/.vnc/xstartup')):
@@ -40,10 +43,12 @@ def setup_desktop():
             os.path.join(HERE, 'share/web/noVNC'),
             '--heartbeat',
             '30',
-            'localhost:{port}',
+            '{port}',
         ]
+        + websockify_args
         + ['--', '/bin/sh', '-c', f'cd {os.getcwd()} && {vnc_command}'],
         'timeout': 30,
         'mappath': {'/': '/vnc.html'},
         'new_browser_window': True,
+        "unix_socket": True,
     }
